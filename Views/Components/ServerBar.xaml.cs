6381636f1f8917873186ba32a0ccb389d9c44c0d
TF Crazy <tfcrazy.dev@gmail.com>
2025-12-23 18:48:47 +0100

    Create ServerBar.xaml.cs

---
+++ Views/Components/ServerBar.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RustControlPanel.Views.Components
+{
+    /// <summary>
+    /// Logique d'interaction pour ServerBar.xaml
+    /// </summary>
+    public partial class ServerBar : UserControl
+    {
+        public ServerBar()
+        {
+            InitializeComponent();
+        }
+    }
+}

70f5bea4c0cb00e501e68231062783036f768653
TF Crazy <tfcrazy.dev@gmail.com>
2025-12-27 12:23:47 +0100

    Update Sidebar.xaml.cs

--- Views/Components/Sidebar.xaml.cs
+++ Views/Components/Sidebar.xaml.cs
@@ -2,8 +2,6 @@ using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
 
 namespace RustControlPanel.Views.Components
 {
@@ -14,38 +12,24 @@ namespace RustControlPanel.Views.Components
         public event EventHandler? PlayersClicked;
         public event EventHandler? ConsoleClicked;
 
-        private const double ExpandedWidth = 200;
-        private const double CollapsedWidth = 64;
         private bool _isCollapsed = false;
 
         public Sidebar()
         {
             InitializeComponent();
-            SetActiveTab(0); // Map par défaut
+            SetActiveTab(0);
         }
 
         private void OnToggleClick(object sender, RoutedEventArgs e)
         {
             _isCollapsed = !_isCollapsed;
-            AnimateWidth(_isCollapsed ? CollapsedWidth : ExpandedWidth);
+            this.Width = _isCollapsed ? 64 : 200;
             UpdateVisibility();
-        }
-
-        private void AnimateWidth(double targetWidth)
-        {
-            var animation = new DoubleAnimation
-            {
-                To = targetWidth,
-                Duration = TimeSpan.FromMilliseconds(250),
-                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
-            };
-            this.BeginAnimation(WidthProperty, animation);
         }
 
         private void UpdateVisibility()
         {
             var visibility = _isCollapsed ? Visibility.Collapsed : Visibility.Visible;
-            
             if (MapText != null) MapText.Visibility = visibility;
             if (StatsText != null) StatsText.Visibility = visibility;
             if (PlayersText != null) PlayersText.Visibility = visibility;
@@ -78,14 +62,14 @@ namespace RustControlPanel.Views.Components
 
         private void SetActiveTab(int index)
         {
-            // Reset all
-            if (MapBorder != null) MapBorder.Background = Brushes.Transparent;
-            if (StatsBorder != null) StatsBorder.Background = Brushes.Transparent;
-            if (PlayersBorder != null) PlayersBorder.Background = Brushes.Transparent;
-            if (ConsoleBorder != null) ConsoleBorder.Background = Brushes.Transparent;
+            if (MapBorder != null) MapBorder.Background = System.Windows.Media.Brushes.Transparent;
+            if (StatsBorder != null) StatsBorder.Background = System.Windows.Media.Brushes.Transparent;
+            if (PlayersBorder != null) PlayersBorder.Background = System.Windows.Media.Brushes.Transparent;
+            if (ConsoleBorder != null) ConsoleBorder.Background = System.Windows.Media.Brushes.Transparent;
 
-            // Set active
-            var activeBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#2800D9FF"));
+            var activeBrush = new System.Windows.Media.SolidColorBrush(
+                (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#2800D9FF"));
+            
             switch (index)
             {
                 case 0: if (MapBorder != null) MapBorder.Background = activeBrush; break;
@@ -93,11 +77,6 @@ namespace RustControlPanel.Views.Components
                 case 2: if (PlayersBorder != null) PlayersBorder.Background = activeBrush; break;
                 case 3: if (ConsoleBorder != null) ConsoleBorder.Background = activeBrush; break;
             }
-        }
-
-        public void SetActive(int index)
-        {
-            SetActiveTab(index);
         }
     }
 }

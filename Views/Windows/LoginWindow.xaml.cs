a8bf37a05b11240c3f4bf1759c29de7b959c8df0
TF Crazy <tfcrazy.dev@gmail.com>
2025-12-27 02:58:33 +0100

    push

--- Views/Windows/LoginWindow.xaml.cs
+++ Views/Windows/LoginWindow.xaml.cs
@@ -25,6 +25,17 @@ namespace RustControlPanel.Views.Windows
         #region Event Handlers
 
         /// <summary>
+        /// Allows dragging the window by the titlebar.
+        /// </summary>
+        private void OnTitleBarMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        /// <summary>
         /// Handles PasswordBox changes to update the ViewModel.
         /// </summary>
         private void OnPasswordChanged(object sender, RoutedEventArgs e)
@@ -32,6 +43,20 @@ namespace RustControlPanel.Views.Windows
             if (DataContext is LoginViewModel vm && sender is PasswordBox pb)
             {
                 vm.Password = pb.Password;
+            }
+        }
+
+        /// <summary>
+        /// Handles double-click on saved server to quick connect.
+        /// </summary>
+        private void OnServerDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (DataContext is LoginViewModel vm && sender is FrameworkElement element)
+            {
+                if (element.DataContext is ServerConfig config)
+                {
+                    vm.QuickConnectCommand.Execute(config);
+                }
             }
         }
 

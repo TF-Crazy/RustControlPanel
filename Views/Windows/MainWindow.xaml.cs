1eafd0d140aefe809b4fc5c318eb27d0d08ab543
TF Crazy <tfcrazy.dev@gmail.com>
2025-12-27 02:05:41 +0100

    push

--- Views/Windows/MainWindow.xaml.cs
+++ Views/Windows/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 // MainWindow.xaml.cs - Main window code-behind
 // ════════════════════════════════════════════════════════════════════
 
+using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 
@@ -33,6 +35,43 @@ namespace RustControlPanel.Views.Windows
                 this.DragMove();
             }
         }
+
+        #region Theme Switcher (DEV ONLY - Remove after choice)
+
+        private void SwitchToGlass(object sender, RoutedEventArgs e)
+        {
+            SwitchTheme("GlassMorphism");
+        }
+
+        private void SwitchToNeuro(object sender, RoutedEventArgs e)
+        {
+            SwitchTheme("Neumorphism");
+        }
+
+        private void SwitchToFluent(object sender, RoutedEventArgs e)
+        {
+            SwitchTheme("Fluent");
+        }
+
+        private void SwitchTheme(string themeName)
+        {
+            var dict = new ResourceDictionary
+            {
+                Source = new System.Uri($"pack://application:,,,/Styles/Themes/Theme.{themeName}.xaml", System.UriKind.Absolute)
+            };
+
+            // Remove old theme
+            var oldTheme = Application.Current.Resources.MergedDictionaries
+                .FirstOrDefault(d => d.Source?.OriginalString.Contains("/Themes/Theme.") == true);
+            
+            if (oldTheme != null)
+                Application.Current.Resources.MergedDictionaries.Remove(oldTheme);
+
+            // Add new theme
+            Application.Current.Resources.MergedDictionaries.Add(dict);
+        }
+
+        #endregion
 
         #endregion
     }
